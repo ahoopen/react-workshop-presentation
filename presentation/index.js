925fe56dc454b343ee838c09c92f4b471642e3fe
ahoopen <auke@frontmen.nl>
2017-01-06 22:41:23 +0100

    Added slides about react components

--- presentation/index.js
+++ presentation/index.js
@@ -83,8 +83,54 @@ export default class Presentation extends Component {
                         </Layout>
                     </Slide>
 
-                    {/*JSX*/}
 
+                    {/*FIRST REACTION*/}
+
+                    <Slide>
+                        <Heading>React first reaction</Heading>
+                        <Layout style={{alignItems: "center", margin: "40px -80px", justifyContent: "space-between"}}>
+                            <Fill>
+                                <CodePane
+                                    lang="jsx"
+                                    textSize=".6em"
+                                    source={require("raw-loader!../assets/react/react.first.example")}
+                                    margin="20px auto"
+                                />
+                            </Fill>
+                            <Fill>
+                                <BlockQuote>
+                                    <Quote textColor="#FFF">WTF?</Quote>
+                                    <Cite>Everyone</Cite>
+                                </BlockQuote>
+                            </Fill>
+                        </Layout>
+                    </Slide>
+
+                    <Slide>
+                        <Text textSize="2.6em" textColor="#FFF" margin="20px 0px 0px">
+                            #1 rule of <Text textColor="#FFF" textSize="1.6em" bold>React</Text>
+                        </Text>
+                        <Appear style={{margin: "40px -80px"}}>
+                            <Text textSize="2.6em" textColor="#FFF">Everything is a
+                                <Text textColor="#FFF" textSize="1.6em" bold>{'<Component />'}</Text>
+                            </Text>
+                        </Appear>
+                    </Slide>
+
+                    <Slide transition={["fade"]} bgColor="#f1f1f1" textColor="primary">
+                        <Heading size={2} textColor="#333">React has no</Heading>
+                        <div style={{margin: "40px -80px"}}>
+                            <Text lineHeight={1.2} bgColor="#fff" textColor="#333" style={{width: "300px", padding: "6px"}}>Controllers</Text>
+                            <Text lineHeight={1.2} bgColor="#fff" textColor="#333" style={{width: "260px", padding: "6px"}}>Templates</Text>
+                            <Text lineHeight={1.2} bgColor="#fff" textColor="#333" style={{width: "400px", padding: "6px"}}>Global event listeners</Text>
+                            <Text lineHeight={1.2} bgColor="#fff" textColor="#333" style={{width: "200px", padding: "6px"}}>Models</Text>
+                        </div>
+                            <Appear>
+                            <Text textSize="2.4em" textColor="#333">Just <Text textSize="1em" bold>{'<Components />'}</Text></Text>
+                        </Appear>
+                    </Slide>
+
+                    {/*JSX*/}
 
                     <Slide transition={["slide"]} bgColor="#333" bgImage={images.jsx.replace("/", "")} bgDarken={0.75}>
                         <Heading size={2} textColor="#FFF" lineHeight={1}>What is JSX</Heading>
@@ -95,10 +141,12 @@ export default class Presentation extends Component {
                                         <ListItem>JSX is a subset/dialect of JavaScript</ListItem>
                                     </Appear>
                                     <Appear>
-                                        <ListItem>It allows us to write what looks like HTML inside our JavaScript</ListItem>
+                                        <ListItem>It allows us to write what looks like HTML inside our
+                                            JavaScript</ListItem>
                                     </Appear>
                                     <Appear>
-                                        <ListItem>Behind the scenes it just JavaScript. it transpiles to regular JavaScript</ListItem>
+                                        <ListItem>Behind the scenes it just JavaScript. it transpiles to regular
+                                            JavaScript</ListItem>
                                     </Appear>
                                 </List>
                             </Fill>
@@ -110,12 +158,13 @@ export default class Presentation extends Component {
                         <Heading size={3} textAlign="left" caps textColor="#FFF" textFont="primary">JSX</Heading>
                         <Layout style={{alignItems: "center", margin: "40px -80px", justifyContent: "space-between"}}>
                             <Fill>
-                                <Text textAlign="left" lineHeight={1.2} textColor="#FFF">JSX elements are treated as JavaScript
+                                <Text textAlign="left" lineHeight={1.2} textColor="#FFF">JSX elements are treated as
+                                    JavaScript
                                     expressions. They can be used anywhere. That means
                                     that a JSX element can:</Text>
                                 <List>
                                     <Appear>
-                                        <ListItem textSize="1.1em">Be saved in a variable</ListItem>
+                                        <ListItem textColor="#FFF" textSize="1.1em">Be saved in a variable</ListItem>
                                     </Appear>
                                     <Appear>
                                         <CodePane
@@ -126,7 +175,8 @@ export default class Presentation extends Component {
                                         />
                                     </Appear>
                                     <Appear>
-                                        <ListItem textSize="1.1em">Stored in a object or array</ListItem>
+                                        <ListItem textColor="#FFF" textSize="1.1em">Stored in a object or
+                                            array</ListItem>
                                     </Appear>
                                     <Appear>
                                         <CodePane
@@ -137,7 +187,7 @@ export default class Presentation extends Component {
                                         />
                                     </Appear>
                                     <Appear>
-                                        <ListItem textSize="1.1em">Passed into a function</ListItem>
+                                        <ListItem textColor="#FFF" textSize="1.1em">Passed into a function</ListItem>
                                     </Appear>
                                 </List>
                             </Fill>
